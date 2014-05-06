d7d4b46954a14542567e7bf269bc01938d16ccb5
Emory Au <emoryau@gmail.com>
2014-05-06 12:18:48 -0400

    Added .png to file blacklist, some cleanup in main file

--- primed/src/gemini_prime_d.cpp
+++ primed/src/gemini_prime_d.cpp
@@ -9,11 +9,11 @@
 #include "TagExtractor.hpp"
 #include "MetadataStore.hpp"
 #include "TrackManager.hpp"
-#include <gst/gst.h>
+#include <glib.h>
 
 #include <iostream>
 
-const char* extension_blacklist[] = { ".jpg", ".cue", ".db", ".m3u", ".ini", ".sfv", ".pdf", ".log", ".txt", NULL };
+const char* extension_blacklist[] = { ".jpg", ".cue", ".db", ".m3u", ".ini", ".sfv", ".pdf", ".log", ".txt", ".png", NULL };
 
 
 bool isInBlacklist( const std::string& subject ) {
@@ -27,29 +27,20 @@ bool isInBlacklist( const std::string& subject ) {
 	return false;
 }
 
-
-int main( int argc, char** argv ) {
-	gst_init (&argc, &argv);
-	
-	const char* folder = "/home/emoryau/testmusic"; // TODO: better default
-	
-	if( argc > 1 )
-		folder = argv[1];
-		
-	Filesystem directory_crawler( folder );
+void updateMetadataStore( const char* directory, const char* store_filename ) {
+	Filesystem directory_crawler( directory );
 	TagExtractor extractor;
 	MetadataStore store;
 	TrackManager trackManager;
 
-	g_print( "Scanning %s\n", folder );
+	g_print( "Scanning %s\n", directory );
 
-	// TODO: Database link
 	// Create some DAO's to talk to the db
 	try {
-		store.open( "/home/emoryau/test.sqlite" );
-	} catch( MetadataStoreException e ) {
-		g_error( "DB opening threw:\n%s", e.what() );
-		return 1;
+		store.open( store_filename );
+	} catch( MetadataStoreException* e ) {
+		g_error( "DB opening threw:\n%s", e->what() );
+		return;
 	}
 
 	for( Filesystem::iterator it = directory_crawler.begin(); it != directory_crawler.end(); ++it ) {
@@ -62,12 +53,21 @@ int main( int argc, char** argv ) {
 		extractor.readTags( filename.c_str() );
 		try {
 			store.addExtractedTrack( extractor );
-		} catch( MetadataStoreException e ) {
-			g_error( "addExtractedTrack() threw:\n%s", e.what() );
+		} catch( MetadataStoreException* e ) {
+			g_error( "addExtractedTrack() threw:\n%s", e->what() );
 		}
-
-		// TODO: insert/update database
 	}
+}
+
+int main( int argc, char** argv ) {
+	gst_init (&argc, &argv);
+
+	const char* folder = "/home/emoryau/testmusic"; // TODO: better default
+
+	if( argc > 1 )
+		folder = argv[1];
+
+	updateMetadataStore( folder, "/home/emoryau/test.sqlite" );
 
 	g_print( "Finished\n" );
 
