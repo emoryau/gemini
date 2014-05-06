9aef819514c72bee69e2ace94dd55194aea8e89e
Emory Au <emoryau@gmail.com>
2014-05-06 12:17:38 -0400

    Fix for empty track names

--- primed/src/Track.cpp
+++ primed/src/Track.cpp
@@ -11,12 +11,16 @@ Track::Track() {
 	artist = NULL;
 	album = NULL;
 	replayGain = -100.0f;
+	trackNumber = 0;
+	discNumber = 1;
 }
 
-Track::Track(TagExtractor& te) {
-	filename.assign(te.getFile_uri());
-	artFilename.assign(""); // TODO: Get art somehow
-	name.assign(te.getTitle());
+Track::Track( TagExtractor& te ) {
+	filename.assign( te.getFile_uri() );
+	artFilename.assign( "" ); // TODO: Get art somehow
+	if( te.getTitle() ) {
+		name.assign( te.getTitle() );
+	}
 	artist = NULL;
 	album = NULL;
 	replayGain = te.getTrackGain();
@@ -28,6 +32,6 @@ Track::~Track() {
 	// TODO Auto-generated destructor stub
 }
 
-bool Track::operator< (const Track &t) const {
+bool Track::operator<( const Track &t ) const {
 	return filename < t.filename;
 }
