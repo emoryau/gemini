1816b88570e44112e5f9122c12fce304f96b8428
unknown <emory.au@nb2077.wdp1.white-directory.com>
2014-05-12 16:21:23 -0400

    Improved command line processing; extensible 'action' framework to run different codepaths. Also error handling and help message output.

--- primed/src/gemini_prime_d.cpp
+++ primed/src/gemini_prime_d.cpp
@@ -14,15 +14,37 @@
 #include <iostream>
 #include <exception>
 #include <string>
+#include <list>
+#include <sstream>
+#include <iomanip>
 
 static const char* extension_blacklist[] = { ".jpg", ".cue", ".db", ".m3u", ".ini", ".sfv", ".pdf", ".log", ".txt", ".png", NULL };
-static const gchar* scan_filename = NULL;
+static const gchar* scan_filename = "/home/emoryau/testmusic";
 static const gchar* database_filename = "/home/emoryau/test.sqlite";
+static gchar** action_commands = NULL;
 
-static GOptionEntry entries[] =
+static GOptionEntry command_line_entries[] =
 {
 		{ "scan", 's', 0, G_OPTION_ARG_FILENAME, &scan_filename, "Scan directory M for music, then exit", "M" },
 		{ "database", 'd', 0, G_OPTION_ARG_FILENAME, &database_filename, "Set name of database to D", "D" },
+		{ G_OPTION_REMAINING, 0, 0, G_OPTION_ARG_STRING_ARRAY, &action_commands, "Action", NULL },
+		{ NULL }
+};
+
+void actionScan( void );
+void actionPlaylist(void);
+void actionDbTest(void);
+
+struct ActionRow {
+	const gchar* command_text;
+	const gchar* help_description;
+	void (*action_func)(void);
+};
+static ActionRow actions[] =
+{
+		{ "scan", "Scan music directory and update database", actionScan },
+		{ "db_test", "Test database", actionDbTest }, // TODO: remove this for production code
+		{ "playlist", "Update base playlist in database", actionPlaylist },
 		{ NULL }
 };
 
@@ -37,17 +59,17 @@ bool isInBlacklist( const std::string& subject ) {
 	return false;
 }
 
-void updateMetadataStore( const char* directory, const char* store_filename ) {
-	Filesystem directory_crawler( directory );
+void actionScan( void ) {
+	Filesystem directory_crawler( scan_filename );
 	TagExtractor extractor;
 	MetadataStore store;
 	DAOFactorySqlite3Impl* daoFactory;
 
-	g_print( "Scanning %s\n", directory );
+	g_print( "Scanning %s\n", scan_filename );
 
 	// Create some DAO's to talk to the db
 	daoFactory = new DAOFactorySqlite3Impl();
-	daoFactory->setDBFile( "/home/emory.au/test.sqlite" );
+	daoFactory->setDBFile( database_filename );
 	store.setDAOFactory( daoFactory );
 
 	for (Filesystem::iterator it = directory_crawler.begin(); it != directory_crawler.end(); ++it) {
@@ -66,44 +88,92 @@ void updateMetadataStore( const char* directory, const char* store_filename ) {
 	}
 }
 
-void parseCommandLine( int argc, char** argv ) {
+void actionPlaylist(void) {
+}
+
+void actionDbTest(void) {
+	try {
+			DAOFactorySqlite3Impl* daoFactory = new DAOFactorySqlite3Impl();
+			daoFactory->setDBFile( database_filename );
+			TrackDAO* trackDAO = daoFactory->getTrackDAO();
+			Track* track = trackDAO->getTrackById( 120 );
+			g_print( "%s - '%s' - %d.%d - %s\n", track->artist ? track->artist->name.c_str() : "<no artist>",
+					track->album ? track->album->name.c_str() : "<no album>", track->discNumber, track->trackNumber, track->name.c_str() );
+			trackDAO->free( track );
+			delete daoFactory;
+		} catch( std::exception* e ) {
+			g_error( e->what() );
+		}
+}
+
+std::list<ActionRow*> parseCommandLine( int argc, char** argv ) {
 	GOptionContext* context;
 	GError* error = NULL;
+	std::stringstream summary_stream;
 
-	context = g_option_context_new( "- music playback daemon" );
-	g_option_context_add_main_entries( context, entries, "" );
+	summary_stream << " Gemini Prime - music playback daemon" << std::endl << std::endl;
+	summary_stream << "Actions available: (at least one required)" << std::endl;
+	summary_stream << std::setfill(' ');
+	for( int i_actions = 0; actions[i_actions].command_text != NULL; i_actions++ ) {
+		summary_stream << std::endl
+			<< std::setw(22) << actions[i_actions].command_text
+			<< std::setw(-1) << " " << actions[i_actions].help_description;
+	}
+
+	context = g_option_context_new( "ACTION_1 ACTION_2 ACTION_N" );
+	g_option_context_set_summary( context, summary_stream.str().c_str() );
+	g_option_context_add_main_entries( context, command_line_entries, NULL );
 	if( !g_option_context_parse( context, &argc, &argv, &error ) ) {
 		g_print( "option parsing failed: %s\n", error->message );
 		exit( 1 );
 	}
+
+	std::list<ActionRow*> requested_actions;
+
+	if( action_commands != NULL ) {
+		gchar** current_action_command = action_commands;
+		while( *current_action_command != NULL ) {
+			int i = 0;
+			while ( actions[i].action_func != NULL ) {
+				if( g_str_has_prefix( actions[i].command_text, *current_action_command ) ) {
+					requested_actions.push_back( &actions[i] );
+					break;
+				}
+				i++;
+			}
+			current_action_command++;
+		}
+	}
+
+	if( requested_actions.size() <= 0 ) {
+		const gchar* help_message = g_option_context_get_help( context, TRUE, NULL );
+		g_print( help_message );
+	}
+
+	g_option_context_free( context );
+
+	return requested_actions;
 }
 
 int main( int argc, char** argv ) {
 	gst_init( &argc, &argv );
 
-	parseCommandLine( argc, argv );
+	std::list<ActionRow*> requested_actions = parseCommandLine( argc, argv );
+
+	if( requested_actions.size() <= 0 ) {
+		g_error( "No action selected\n" );
+		exit( 1 );
+	}
 
 	g_print( "Database filename: %s\n", database_filename );
 
-	if( scan_filename != NULL ) {
-		updateMetadataStore( scan_filename, database_filename );
-		exit(0);
+	for( std::list<ActionRow*>::iterator action = requested_actions.begin(); action != requested_actions.end(); action++ ) {
+		g_print( "Starting requested_actions: %s\n", (*action)->command_text );
+		if( (*action)->action_func != NULL ) {
+			(*action)->action_func();
+		}
+		g_print( "Finished %s\n", (*action)->command_text );
 	}
-
-	try {
-		DAOFactorySqlite3Impl* daoFactory = new DAOFactorySqlite3Impl();
-		daoFactory->setDBFile( database_filename );
-		TrackDAO* trackDAO = daoFactory->getTrackDAO();
-		Track* track = trackDAO->getTrackById( 120 );
-		g_print( "%s - '%s' - %d.%d - %s\n", track->artist ? track->artist->name.c_str() : "<no artist>",
-				track->album ? track->album->name.c_str() : "<no album>", track->discNumber, track->trackNumber, track->name.c_str() );
-		trackDAO->free( track );
-		delete daoFactory;
-	} catch( std::exception* e ) {
-		g_error( e->what() );
-	}
-
-	g_print( "Finished\n" );
 
 	return 0;
 }
