115f82be3cee6198f10c3ea97a062326568b46ed
unknown <emory.au@nb2077.wdp1.white-directory.com>
2014-05-16 16:07:09 -0400

    Cleaned up gemini_prime_d, removed action code

--- primed/src/gemini_prime_d.cpp
+++ primed/src/gemini_prime_d.cpp
@@ -7,118 +7,24 @@
 
 #include <glib.h>
 #include <stdlib.h>
-#include <iostream>
-#include <exception>
 #include <string>
 #include <list>
-#include <sstream>
-#include <iomanip>
 
-#include "Filesystem.hpp"
-#include "TagExtractor.hpp"
-#include "MetadataStore.hpp"
-#include "PlaylistService.hpp"
 #include "dao/DAOFactory.hpp"
 #include "TestController.hpp"
+#include "MaintenanceController.hpp"
 
-static const char* extension_blacklist[] = { ".jpg", ".cue", ".db", ".m3u", ".ini", ".sfv", ".pdf", ".log", ".txt", ".png", NULL };
-static const gchar* scan_filename_default = "/home/emoryau/testmusic";
 static const gchar* database_URI_default = "sqlite3:///home/emoryau/test.sqlite";
 static gchar* database_URI = NULL;
-static gchar* scan_filename = NULL;
-static gchar** action_commands = NULL;
 
 typedef std::list<Controller*> ControllerList;
 static ControllerList controllers;
 
 static GOptionEntry command_line_entries[] =
 {
-		{ "scan_filename", 's', 0, G_OPTION_ARG_FILENAME, scan_filename, "Set music scan directory M", "M" },
 		{ "database_uri", 'd', 0, G_OPTION_ARG_FILENAME, &database_URI, "Set name of database to D", "D" },
-		{ G_OPTION_REMAINING, 0, 0, G_OPTION_ARG_STRING_ARRAY, &action_commands, "Action", NULL },
 		{ NULL }
 };
-
-static void actionScan( void );
-void actionPlaylist(void);
-void actionTestDb(void);
-
-struct ActionRow {
-	const gchar* command_text;
-	const gchar* help_description;
-	void (*action_func)(void);
-};
-static ActionRow actions[] =
-{
-		{ "scan", "Scan music directory and update database", actionScan },
-		{ "playlist", "Update base playlist in database", actionPlaylist },
-		{ "test_db", "Test database", actionTestDb }, // TODO: remove this for production code
-		{ NULL }
-};
-
-bool isInBlacklist( const std::string& subject ) {
-	const char** extension_comparator = extension_blacklist;
-	while( *extension_comparator != NULL ) {
-		if( subject.rfind( *extension_comparator ) != std::string::npos ) {
-			return true;
-		}
-		extension_comparator++;
-	}
-	return false;
-}
-
-void actionScan( void ) {
-	Filesystem directory_crawler( scan_filename );
-	TagExtractor extractor;
-	MetadataStore store;
-	DAOFactory* daoFactory = DAOFactory::CreateDAOFactoryFromURI( database_URI );
-
-	g_print( "Scanning %s\n", scan_filename );
-
-	store.setDAOFactory( daoFactory );
-
-	for (Filesystem::iterator it = directory_crawler.begin(); it != directory_crawler.end(); ++it) {
-		// Parse file
-		std::string filename = *it;
-
-		if( isInBlacklist( filename ) )
-			continue;
-
-		extractor.readTags( filename.c_str() );
-		store.addExtractedTrack( extractor );
-	}
-}
-
-void actionPlaylist(void) {
-	try {
-		DAOFactory* daoFactory = DAOFactory::CreateDAOFactoryFromURI( database_URI );
-		PlaylistService* playlistService = new PlaylistService( daoFactory );
-
-		playlistService->refreshEverythingPlaylist();
-
-		delete playlistService;
-		delete daoFactory;
-	} catch (std::exception* ex ) {
-		g_error( ex->what() );
-	}
-}
-
-void actionTestDb(void) {
-	try {
-			DAOFactory* daoFactory = DAOFactory::CreateDAOFactoryFromURI( database_URI );
-			daoFactory->setDBFile( database_URI );
-			TrackDAO* trackDAO = daoFactory->getTrackDAO();
-			Track criterion;
-			criterion.id = 120;
-			Track* track = trackDAO->getTrack( &criterion );
-			track->print();
-			trackDAO->free( track );
-			delete daoFactory;
-		} catch( std::exception* e ) {
-			g_error( e->what() );
-		}
-}
-
 
 
 void parseCommandLine( int argc, char** argv ) {
@@ -144,14 +50,12 @@ int main( int argc, char** argv ) {
 	gst_init( &argc, &argv );
 
 	controllers.push_back( new TestController );
+	controllers.push_back( new MaintenanceController );
 
 	parseCommandLine( argc, argv );
 
 	if( database_URI == NULL ) {
 		database_URI = g_strdup( database_URI_default );
-	}
-	if( scan_filename == NULL ) {
-		scan_filename = g_strdup( scan_filename_default );
 	}
 
 	DAOFactory* dao_factory = DAOFactory::CreateDAOFactoryFromURI( database_URI );
@@ -171,13 +75,6 @@ int main( int argc, char** argv ) {
 
 	delete dao_factory;
 	g_free( database_URI );
-	g_free( scan_filename );
 
 	return 0;
-}
-
-void printExtractorContents( const TagExtractor& extractor ) {
-	std::cout << extractor.getArtist() << "\t" << extractor.getAlbum() << "\t" << extractor.getTitle() << "\n";
-	std::cout << extractor.getDiscNumber() << "\t" << extractor.getTrackNumber() << "\t" << extractor.getAlbumGain() << "\t"
-			<< extractor.getTrackGain() << "\n";
 }
